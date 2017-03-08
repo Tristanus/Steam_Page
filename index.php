ea9e732ed3ca37901c82d2ff2cc69026df35cd38
DESKTOP-S0MLQRE\Tristan <info@tristandejager.com>
2017-03-08 14:21:43 +0100

    new background. song quality better.

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+
+	<head>
+		<title>Tristanus</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="UTF-8">
+	</head>
+
+	<body>
+	
+		<div class="wrapper">
+			<br>
+			<p class="title"></p>
+			<div class="headline">
+				<h1>Tristanus</h1>
+			</div>		
+			<video loop autoplay id="bgvid">
+				<source src="bg.mp4" type="video/mp4">
+			</video>	
+
+			<audio loop autoplay> 
+				<source src="audio.mp3" />
+			</audio>
+		</div>
+			
+	</body>
+
+</html>
